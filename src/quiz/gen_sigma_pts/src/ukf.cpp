5ce11e94d5ab6a0575d2f745a13472214f6c979b
Endymion Cheung <endymion.cheung@gmail.com>
2020-03-13 00:05:49 -0400

    Class Exercise: Generating Sigma Points Assignment

---
+++ src/quiz/gen_sigma_pts/src/ukf.cpp
@@ -0,0 +1,86 @@
+#include "ukf.h"
+#include <iostream>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+UKF::UKF() {
+  Init();
+}
+
+UKF::~UKF() {
+
+}
+
+void UKF::Init() {
+
+}
+
+/**
+ * Programming assignment functions: 
+ */
+void UKF::GenerateSigmaPoints(MatrixXd* Xsig_out) {
+
+  // set state dimension
+  int n_x = 5;
+
+  // define spreading parameter
+  double lambda = 3 - n_x;
+
+  // set example state
+  VectorXd x = VectorXd(n_x);
+  x <<   5.7441,
+         1.3800,
+         2.2049,
+         0.5015,
+         0.3528;
+
+  // set example covariance matrix
+  MatrixXd P = MatrixXd(n_x, n_x);
+  P <<     0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
+          -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
+           0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
+          -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
+          -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
+
+  // create sigma point matrix
+  MatrixXd Xsig = MatrixXd(n_x, 2 * n_x + 1); // 5 rows and 11 cols
+
+  // calculate square root of P
+  // squre root matrix of the covariance matrix P
+  // Cholesky decomposition
+  MatrixXd A = P.llt().matrixL();
+
+  /**
+   * Student part begin
+   */
+
+  // your code goes here 
+  // calculate sigma points ...
+  // calculate 11 sigma points and fill the columns of Xsig with sigma points
+  // make sure use the sme ordering of sigma points as showed in the video to make sure the evaluation works
+
+
+  // set sigma points as columns of matrix Xsig
+
+  /**
+   * Student part end
+   */
+
+  // print result
+  // std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+
+  // write result
+  // putting the results of matrix by receiving the pointer Xsig_out as in put and points to a matrix
+  *Xsig_out = Xsig;
+}
+
+/**
+ * expected result:
+ * Xsig =
+ *  5.7441  5.85768   5.7441   5.7441   5.7441   5.7441  5.63052   5.7441   5.7441   5.7441   5.7441
+ *    1.38  1.34566  1.52806     1.38     1.38     1.38  1.41434  1.23194     1.38     1.38     1.38
+ *  2.2049  2.28414  2.24557  2.29582   2.2049   2.2049  2.12566  2.16423  2.11398   2.2049   2.2049
+ *  0.5015  0.44339 0.631886 0.516923 0.595227   0.5015  0.55961 0.371114 0.486077 0.407773   0.5015
+ *  0.3528 0.299973 0.462123 0.376339  0.48417 0.418721 0.405627 0.243477 0.329261  0.22143 0.286879
+ */
