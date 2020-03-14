2bffb5846f6082a3ce1a22ed6697b648459e49a3
Endymion Cheung <endymion.cheung@gmail.com>
2020-03-14 18:04:32 -0400

    Implemented sigma point prediction class solution

--- src/quiz/sigmaPointPrediction/src/ukf.cpp
+++ src/quiz/sigmaPointPrediction/src/ukf.cpp
@@ -24,10 +24,10 @@ void UKF::Init() {
 void UKF::SigmaPointPrediction(MatrixXd* Xsig_out) {
 
   // set state dimension
-  int n_x = 5;
+  const int n_x = 5;
 
   // set augmented dimension
-  int n_aug = 7;
+  const int n_aug = 7;
 
   // create example sigma point matrix
   MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
@@ -43,18 +43,59 @@ void UKF::SigmaPointPrediction(MatrixXd* Xsig_out) {
   // create matrix with predicted sigma points as columns
   MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
 
-  double delta_t = 0.1; // time diff in sec
+  const double delta_t = 0.1; // time diff in sec
 
   /**
    * Student part begin
    */
 
+  const float CLOSE_TO_ZERO = 0.001;
   // predict sigma points
+  for (int i = 0; i< 2*n_aug + 1; ++i) {
+    
+    // extract values for better readability
+    double p_x         = Xsig_aug(0,i); // x-position
+    double p_y         = Xsig_aug(1,i); // y-positiom
+    double v           = Xsig_aug(2,i); // velocity
+    double yaw         = Xsig_aug(3,i); // yaw angle
+    double yaw_dot     = Xsig_aug(4,i); // yaw rate
+    double nu_a        = Xsig_aug(5,i); // acceleration noise
+    double nu_yaw_ddot = Xsig_aug(6,i); // yaw rate noise
 
-  // avoid division by zero
+    // predict x and y position of the state values
+    double p_x_pred, p_y_pred;
+    // avoid division by zero when zero yaw rate (`yaw_dot`) is used
+    // for predicting the x and y position of sigma point
+    if (fabs(yaw_dot) > CLOSE_TO_ZERO) {
+        // predict x and y when yaw rate is not zero
+        p_x_pred = p_x + v/yaw_dot * ( sin(yaw + yaw_dot*delta_t) - sin(yaw) );
+        p_y_pred = p_y + v/yaw_dot * ( cos(yaw) - cos(yaw+yaw_dot*delta_t) );
+    } else {
+        // special case when yaw rate is zero
+        p_x_pred = p_x + v * delta_t * cos(yaw);
+        p_y_pred = p_y + v * delta_t * sin(yaw);
+    }
 
-  // write predicted sigma points into right column
+    // predict the velocity, yaw amgle and yaw rate of sigma point
+    double v_pred = v;
+    double yaw_pred = yaw + yaw_dot*delta_t;
+    double yaw_dot_pred = yaw_dot;
 
+    // add process noise to the predicted sigma point
+    p_x_pred = p_x_pred + 0.5 * delta_t*delta_t * nu_a * cos(yaw);
+    p_y_pred = p_y_pred + 0.5 * delta_t*delta_t * nu_a * sin(yaw);
+    v_pred = v_pred + delta_t * nu_a;
+    yaw_pred = yaw_pred + 0.5 * delta_t*delta_t * nu_yaw_ddot;
+    yaw_dot_pred = yaw_dot_pred + delta_t * nu_yaw_ddot;
+
+    // write predicted sigma point into right column
+    Xsig_pred(0,i) = p_x_pred;
+    Xsig_pred(1,i) = p_y_pred;
+    Xsig_pred(2,i) = v_pred;
+    Xsig_pred(3,i) = yaw_pred;
+    Xsig_pred(4,i) = yaw_dot_pred;
+  }
+  
   /**
    * Student part end
    */
