78c777f45f2f8d387e851d0438dc0488c91832d7
Endymion Cheung <endymion.cheung@gmail.com>
2020-03-14 22:18:20 -0400

    Class Exercise: Predicted Mean and Covariance
    Assignment: completing the missing code in ukf.cpp, `PredictMeanAndCovariance()` function.
    
    Tasks:
    
    - set weights
    - predict state mean
    - predict state covariance matrix

---
+++ src/quiz/PredictMeanAndCovariance/src/solutions/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "../Eigen/Dense"
+#include "ukf.h"
+
+using Eigen::MatrixXd;
+
+int main() {
+
+  // Create a UKF instance
+  UKF ukf;
+
+  /**
+   * Programming assignment calls
+   */
+  VectorXd x_pred = VectorXd(5);
+  MatrixXd P_pred = MatrixXd(5, 5);
+  ukf.PredictMeanAndCovariance(&x_pred, &P_pred);
+
+  return 0;
+}
