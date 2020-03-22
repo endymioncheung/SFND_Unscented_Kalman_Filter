0c637d5025434883903fcf75951860dfd3e20fc9
Endymion Cheung <endymion.cheung@gmail.com>
2020-03-22 12:10:59 -0400

    WIP: UKF initialization
    Work in progress Unscented Kalman Filter initialization. Currently unable to run the `./ukf_highway`.
    
    ./ukf_highway
    ukf_highway: /usr/include/eigen3/Eigen/src/Core/DenseCoeffsBase.h:413: Eigen::DenseCoeffsBase<Derived, 1>::Scalar& Eigen::DenseCoeffsBase<Derived, 1>::operator()(Eigen::Index) [with Derived = Eigen::Matrix<double, -1, 1>; Eigen::DenseCoeffsBase<Derived, 1>::Scalar = double; Eigen::Index = long int]: Assertion `index >= 0 && index < size()' failed.
    Aborted (core dumped)

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "ukf.h"
 #include "Eigen/Dense"
 
@@ -54,6 +55,32 @@ UKF::UKF() {
    * TODO: Complete the initialization. See ukf.h for other member properties.
    * Hint: one or more values initialized above might be wildly off...
    */
+
+   // Default initialization to false and set to true in first call of ProcessMeasurement
+  is_initialized_ = false;
+
+  // State dimension
+  n_x_ = 5;
+  
+  // Augmented state dimension
+  n_aug_ = 7;
+
+  // Sigma point spreading parameter
+  lambda_ = 3 - n_aug_;
+
+  // Initalize 1x5 state vector
+  x_.fill(0);
+
+  // Set 5x5 covariance matrix to identity matrix
+  P_.setIdentity(n_x_,n_x_);
+
+  // set vector for weights of sigma points
+  double weight = 0.5 / (lambda_+n_aug_);
+  weights_(0)   = lambda_/(lambda_+n_aug_);;
+
+  for (int i=1; i < 2*n_aug_ + 1; ++i) {  
+    weights_(i) = weight;
+  }
 }
 
 UKF::~UKF() {}
@@ -63,6 +90,44 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
    * TODO: Complete this function! Make sure you switch between lidar and radar
    * measurements.
    */
+
+  // state vector, x_ expects [pos1 pos2 vel_abs yaw_angle yaw_rate] in SI units and rad
+
+  // Set measurement timestamp
+  time_us_ = meas_package.timestamp_ ;
+
+  // Process first measurement from either lidar and radar
+  if (!is_initialized_) {
+
+    // Process measurement from lidar sensor
+    if (meas_package.sensor_type_ == MeasurementPackage::LASER)
+    {
+      double px = meas_package.raw_measurements_(0);       // x-position
+      double py = meas_package.raw_measurements_(1);       // y-position
+      x_ << px, py, 0, 0, 0;
+    }
+    // Processs measurement from radar sensor
+    else if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+      double rho = meas_package.raw_measurements_(0);      // range
+      double phi = meas_package.raw_measurements_(1);      // bearing
+      double rho_dot = meas_package.raw_measurements_(2);  // change of range
+
+      // Transform radar's polar coordinate to Cartesian coordinate
+      double px = rho * cos(phi);      // x-position
+      double py = rho * sin(phi);      // y-position
+      double vx = rho_dot * cos(phi);  // x-component of velocity
+      double vy = rho_dot * sin(phi);  // y-component of velocity
+      double v = sqrt(vx*vx + vy*vy);  // absolute velocity
+      x_ << px, py, v, 0, 0;
+    } else {
+      std::cerr << "Invalid sensor type!" << std::endl;
+      return;
+    }
+
+    // First call measurement initalization completed
+    is_initialized_ = true;
+    return;
+  }
 }
 
 void UKF::Prediction(double delta_t) {
@@ -71,6 +136,12 @@ void UKF::Prediction(double delta_t) {
    * Modify the state vector, x_. Predict sigma points, the state, 
    * and the state covariance matrix.
    */
+
+  // Estimate the object's location
+  // Modify state vector, x_
+  // Predict sigma points
+  // Predict state
+  // Predict state covariance matrix
 }
 
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
