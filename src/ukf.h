9ddddf876c81cb3e2ccef4c66dc95795246052fc
Endymion Cheung <endymion.cheung@gmail.com>
2020-03-24 04:40:22 -0400

    Added comments to functions

--- src/ukf.h
+++ src/ukf.h
@@ -41,16 +41,79 @@ class UKF {
    */
   void UpdateRadar(MeasurementPackage meas_package);
 
+  /**
+   * Normalize the angle within +/- pi
+   * @param angle_vector Vector containing angle in rad
+   * @param angle_col    column index of the angle
+   */
   void AngleNormalization(Eigen::VectorXd &angle_vector, int angle_col);
+
+  /**
+   * Generate the sigma point matrix for given state vector and covariance matrix
+   * @param x State vector
+   * @param P Covariance matrix
+   * @param x Sigma point matrix
+   */
   void GenerateSigmaPoints(Eigen::VectorXd &x, Eigen::MatrixXd &P, Eigen::MatrixXd &Xsig);
+
+  /**
+   * Augment sigma points
+   * @param x_aug     Augmented mean state
+   * @param P_aug     Augmented covariance matrix
+   * @param X_sig_aug Augmented sigma point matrix
+   */
   void AugmentedSigmaPoints(Eigen::VectorXd &x_aug, Eigen::MatrixXd &P_aug, Eigen::MatrixXd &Xsig_aug);
+
+  /**
+   * Sigma points prediction
+   * @param X_sig_aug Augmented sigma point matrix
+   * @param deta_t    Time between k and k+1 in s
+   * @param Xsig_pred Predicted sigma points matrix as columns in state space
+   */
   void SigmaPointPrediction(Eigen::MatrixXd &Xsig_aug, double delta_t, Eigen::MatrixXd &Xsig_pred);
+
+  /**
+   * Predict mean and covariance matrix
+   * @param Xsig_pred Predicted sigma points matrix as columns in state space
+   * @param x_aug     Augmented mean state
+   * @param P_aug     Augmented covariance matrix 
+   */
   void PredictMeanAndCovariance(Eigen::MatrixXd &Xsig_pred, Eigen::VectorXd &x, Eigen::MatrixXd &P);
   
+  /**
+   * Predict radar measurement
+   * @param Xsig_pred   Predicted sigma points matrix as columns in state space
+   * @param Z_radar_sig Radar sigma points matrix in measurement space
+   * @param z_pred      Predicted radar measurement mean
+   * @param S           Predicted radar measurement covariance matrix
+   */
   void PredictRadarMeasurement(Eigen::MatrixXd &Xsig_pred, Eigen::MatrixXd &Z_radar_sig, Eigen::VectorXd &z_pred, Eigen::MatrixXd &S);
+
+  /**
+   * Update radar state
+   * @param z_radar   Radar measurement
+   * @param x         Radar sigma points matrix in measurement space
+   * @param P         Predicted radar measurement mean
+   * @param NIS_radar Predicted radar measurement covariance matrix
+   */
   void UpdateRadarState(Eigen::VectorXd &z_radar, Eigen::VectorXd &x, Eigen::MatrixXd &P, Eigen::MatrixXd &NIS_radar);
 
+  /**
+   * Predict lidar measurement
+   * @param Xsig_pred   Predicted sigma points matrix as columns in state space
+   * @param Z_lidar_sig Lidar sigma points matrix in measurement space
+   * @param z_pred      Predicted lidar measurement mean
+   * @param S           Predicted lidar measurement covariance matrix
+   */
   void PredictLidarMeasurement(Eigen::MatrixXd &Xsig_pred, Eigen::MatrixXd &Z_lidar_sig, Eigen::VectorXd &z_pred, Eigen::MatrixXd &S);
+
+  /**
+   * Update lidar state
+   * @param z_liar    Lidar measurement
+   * @param x         Lidar sigma points matrix in measurement space
+   * @param P         Predicted lidar measurement mean
+   * @param NIS_lidar Predicted lidar measurement covariance matrix
+   */
   void UpdateLidarState(Eigen::VectorXd &z_lidar, Eigen::VectorXd &x, Eigen::MatrixXd &P, Eigen::MatrixXd &NIS_lidar);
 
   // if this is false, laser measurements will be ignored (except for init)
